b5a998d03882ce0afbc9fb3b91a81f9647871cde
rairishabh <mailmerairishabh99@gmail.com>
2022-10-16 14:15:24 +0530

    first commit

---
+++ student-info/src/components/Header.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+function Header() {
+    return(
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">age</th>
+          <th scope="col">gender</th>
+          <th scope="col">email</th>
+          <th scope="col">Phone Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Rishabh</td>
+          <td>Rai</td>
+          <td>22</td>
+          <td>Male</td>
+          <td>email</td>
+          <td>8850293457</td>
+        </tr>
+        <tr>
+          <th scope="row">1</th>
+          <td>Rishabh</td>
+          <td>Rai</td>
+          <td>22</td>
+          <td>Male</td>
+          <td>email</td>
+          <td>8850293457</td>
+        </tr>
+        <tr>
+          <th scope="row">1</th>
+          <td>Rishabh</td>
+          <td>Rai</td>
+          <td>22</td>
+          <td>Male</td>
+          <td>email</td>
+          <td>8850293457</td>
+        </tr>
+        <tr>
+          <th scope="row">1</th>
+          <td>Rishabh</td>
+          <td>Rai</td>
+          <td>22</td>
+          <td>Male</td>
+          <td>email</td>
+          <td>8850293457</td>
+        </tr>
+        <tr>
+          <th scope="row">1</th>
+          <td>Rishabh</td>
+          <td>Rai</td>
+          <td>22</td>
+          <td>Male</td>
+          <td>email</td>
+          <td>8850293457</td>
+        </tr>
+      </tbody>
+    </table>
+    );
+  }
+export default Header;
